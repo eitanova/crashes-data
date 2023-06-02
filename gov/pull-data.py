b9bd93309042221e5c8077294b5ad35c5c8a7c86
eitanova <84419166+eitanova@users.noreply.github.com>
2023-06-02 19:30:45 +0300

    Added new resource to API call

--- gov/pull-data.py
+++ gov/pull-data.py
@@ -33,36 +33,55 @@ def convert_to_city_name(row_name, df):
 
 
 def main():
+    # Gov variables
     gov_url = "https://data.gov.il/api/3/action/datastore_search?resource_id="
     gov_resources = {
-        'area': '57c5aef9-70f9-4b71-82fa-52304cfbd031',
+        'area': ['b17b1634-c001-4d37-96c6-a661b2ecd98c'],
         'year': ['ecf37201-e71a-4af5-99a5-062fdf07a38d', 'f372a398-e2c1-408c-8333-e758cf2124c5']
     }
     gov_resources_params = {
         'year': ['MEZEG_AVIR', 'SEMEL_YISHUV', "SHNAT_TEUNA", "HODESH_TEUNA", "SUG_YOM", "YOM_LAYLA",
-                 "YOM_BASHAVUA", "HUMRAT_TEUNA", "SUG_TEUNA" ]
+                 "YOM_BASHAVUA", "HUMRAT_TEUNA", "SUG_TEUNA"],
+        'area': ['CITYCODE', 'YEARMONTH']
     }
-
     gov_new_params_name = {
-       'MEZEG_AVIR': 'weather',
-        'SEMEL_YISHUV': 'city',
-        'SHNAT_TEUNA': "year",
-        'HODESH_TEUNA': 'month',
-        'SUG_YOM': 'day_type',
-        'YOM_LAYLA': 'day_time',
-        'YOM_BASHAVUA': 'day',
-        'HUMRAT_TEUNA': 'accident_severity',
-        'SUG_TEUNA': 'accident_type'
+        'year':
+        {
+            'MEZEG_AVIR': 'weather',
+            'SEMEL_YISHUV': 'city',
+            'SHNAT_TEUNA': "year",
+            'HODESH_TEUNA': 'month',
+            'SUG_YOM': 'day_type',
+            'YOM_LAYLA': 'day_time',
+            'YOM_BASHAVUA': 'day',
+            'HUMRAT_TEUNA': 'accident_severity',
+            'SUG_TEUNA': 'accident_type'
+        },
+        'area':
+        {
+            'CITYCODE': 'city',
+            'YEARMONTH': 'year'
+        }
     }
-    merged_df = pd.DataFrame()
 
-    for gov_resource in gov_resources['year']:
-        df = retrieve_records(gov_url, gov_resource, gov_resources_params['year'])
-        merged_df = pd.concat([merged_df, df], ignore_index=True)
+    gov_data = pd.DataFrame()
 
-    merged_df = convert_to_city_name('SEMEL_YISHUV', merged_df)
-    merged_df = merged_df.rename(columns=gov_new_params_name)
-    print(merged_df.columns)
+    # Request all data from Gov API & store in 'gov_data' DF
+    for key in gov_resources.keys():
+        for gov_resource in gov_resources[key]:
+            df = retrieve_records(gov_url, gov_resource, gov_resources_params[key])
+            df = df.rename(columns=gov_new_params_name[key])
+
+            print(df.shape)
+            # Data manipulation & adjustment for all requests from 'area'
+            if key == 'area':
+                df['month'] = df['year'].str[4:]
+                df['year'] = df['month'].str[:4]
+
+            gov_data = pd.concat([gov_data, df], ignore_index=True)
+
+    gov_data = convert_to_city_name('city', gov_data)
+    print(gov_data.shape)
 
 
 if __name__ == '__main__':
