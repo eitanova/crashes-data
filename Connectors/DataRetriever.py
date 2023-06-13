8807120f75b46d4ee2662e4d7e8cf4cba2a4c594
eitanova <84419166+eitanova@users.noreply.github.com>
2023-06-13 23:45:11 +0300

    Add extract data from zip files

--- Connectors/DataRetriever.py
+++ Connectors/DataRetriever.py
@@ -1,5 +1,8 @@
+import os
+
 import requests
 import pandas as pd
+import zipfile
 
 
 class DataRetriever:
@@ -23,6 +26,26 @@ class DataRetriever:
             return 200
 
         return 500
+
+    def extract_data_zip(self, zip_path, zip_name, csv_file, selected_params):
+
+        response = requests.get(zip_path)
+
+        with open(zip_name, 'wb') as f:
+            f.write(response.content)
+
+        with zipfile.ZipFile(zip_name, 'r') as zip_ref:
+            zip_ref.extract(csv_file)
+
+        data = pd.read_csv(csv_file)
+
+        if selected_params:
+            data = data[selected_params]
+
+        os.remove(csv_file)
+        os.remove(zip_name)
+
+        self.df_data = pd.concat([self.df_data, data], ignore_index=True)
 
     def rename_data(self, new_params):
         for old_name, new_name in new_params.items():
