6d1acd34d81b1293e609934f335e26ed72fc9969
MAor <206432718@idf.il>
2023-06-14 14:11:15 +0300

    maor changes

--- gov/pull-data.py
+++ gov/pull-data.py
@@ -1,5 +1,9 @@
 import requests
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split ,GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
 
 
 # Create API request and returns answer as DataFrame with the selected parameters
@@ -31,6 +35,31 @@ def convert_to_city_name(row_name, df):
 
     return df
 
+
+# Delete all the line with Nan values and not relevant values
+def clean_data(gov_data):
+    gov_data = gov_data[gov_data['year'].apply(lambda x: len(str(x)) == 4)]
+    gov_data = gov_data.dropna(subset=['year'])
+    gov_data = gov_data.dropna(subset=['month'])
+    gov_data = gov_data.dropna(subset=['accident_severity'])
+    return gov_data
+
+# use LogisticRegression to apply LogisticRegression on the data
+def apply_LogisticRegression(gov_data):
+    logisticreg = LogisticRegression()
+    X = gov_data[['year','month','weather','day_type','day_time','day']]
+    y = gov_data['accident_severity']
+    x_train, x_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state = 98)
+    logisticreg.fit(x_train, y_train)
+    y_pred = logisticreg.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy ,y_test, y_pred
+
+# build confusion matrix on the resukts from the LogisticRegression
+def build_confusion_matrix(y_test, y_pred):
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    cm_df = pd.DataFrame(cm)
+    return cm_df
 
 def main():
     # Gov variables
@@ -80,9 +109,14 @@ def main():
 
             gov_data = pd.concat([gov_data, df], ignore_index=True)
 
-    gov_data = convert_to_city_name('city', gov_data)
+    # gov_data = convert_to_city_name('city', gov_data)
     print(gov_data.shape)
 
+    gov_data = clean_data(gov_data)
+    accuracy ,y_test, y_pred = apply_LogisticRegression(gov_data)
+    cm_df = build_confusion_matrix(y_test, y_pred)
+    print(cm_df)
+    print(accuracy)
 
 if __name__ == '__main__':
     main()
